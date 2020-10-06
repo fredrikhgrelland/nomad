9b90f5a07278e1cde97d6dbcb3756f5d2e7e36c6
davemay99 <dmay@hashicorp.com>
2020-10-06 09:47:16 -0400

    update deprecated syntax per GH-9027

--- command/operator_debug_test.go
+++ command/operator_debug_test.go
@@ -37,7 +37,7 @@ func TestDebugSuccesses(t *testing.T) {
 	srv, _, _ := testServer(t, false, nil)
 	defer srv.Shutdown()
 
-	ui := new(cli.MockUi)
+	ui := cli.NewMockUi()
 	cmd := &OperatorDebugCommand{Meta: Meta{Ui: ui}}
 
 	// NOTE -- duration must be shorter than default 2m to prevent testify from timing out

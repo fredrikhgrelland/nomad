119527aa30f56fc2861ae52b202bed264045a54b
Michael Schurter <mschurter@hashicorp.com>
2020-10-08 22:21:41 -0700

    api: add field filters to /v1/{allocations,nodes}
    Fixes #9017
    
    The ?resources=true query parameter includes resources in the object
    stub listings. Specifically:
    
    - For `/v1/nodes?resources=true` both the `NodeResources` and
      `ReservedResources` field are included.
    - For `/v1/allocations?resources=true` the `AllocatedResources` field is
      included.
    
    The ?task_states=false query parameter removes TaskStates from
    /v1/allocations responses. (By default TaskStates are included.)

--- nomad/state/state_store.go
+++ nomad/state/state_store.go
@@ -2552,7 +2552,7 @@ func (s *StateStore) CSIPluginDenormalize(ws memdb.WatchSet, plug *structs.CSIPl
 		if alloc == nil {
 			continue
 		}
-		plug.Allocations = append(plug.Allocations, alloc.Stub())
+		plug.Allocations = append(plug.Allocations, alloc.Stub(nil))
 	}
 
 	return plug, nil
